265fc361171a6951fc7e326b92191d9c78d819fe
passiveentity <dk26.01.1999@yandex.ru>
2023-11-17 13:14:39 +0300

    RJVTYN2

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-int a = 12;
-Console.WriteLine(a);
-Console.WriteLine("123");
+
+string gh = "GHGHJG";
+double i = 13;
+Console.WriteLine(gh);
+Console.WriteLine(i);
